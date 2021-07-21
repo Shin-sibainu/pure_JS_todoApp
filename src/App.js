2ab24d206757741e2118781ee3e7e249c7850ae6
Shin-sibainu <shincode0712@gmail.com>
2021-07-21 10:12:25 +0900

    changed for youtube

--- src/App.js
+++ src/App.js
@@ -127,7 +127,24 @@ export class App {
 
     //ここからはYotube用
     /* 
-    
+    const containerElement = document.querySelector("#js-todo-list");
+        const todoItemCountElement = document.querySelector("#js-todo-count");
+        // Todoアイテム数
+        let todoItemCount = 0;
+        formElement.addEventListener("submit", (event) => {
+            // 本来のsubmitイベントの動作を止める
+            event.preventDefault();
+            // 追加するTodoアイテムの要素(li要素)を作成する
+            const todoItemElement = element`<li>${inputElement.value}</li>`;
+            // Todoアイテムをcontainerに追加する
+            containerElement.appendChild(todoItemElement);
+            // Todoアイテム数を+1し、表示されてるテキストを更新する
+            todoItemCount += 1;
+            todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+            // 入力欄を空文字列にしてリセットする
+            inputElement.value = "";
+        });
+    }
     */
   }
 
